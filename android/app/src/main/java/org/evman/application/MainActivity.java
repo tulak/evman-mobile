375328696b9a5e58c0d89ef8564ab6aee100e0a6
Filip Zachar <tulak45@gmail.com>
2018-12-11 13:11:08 +0100

    Rename Android application ID

--- android/app/src/main/java/org/evman/application/MainActivity.java
+++ android/app/src/main/java/org/evman/application/MainActivity.java
@@ -1,4 +1,4 @@
-package com.evman;
+package org.evman.application;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/org/evman/application/MainApplication.java
+++ android/app/src/main/java/org/evman/application/MainApplication.java
@@ -1,4 +1,4 @@
-package com.evman;
+package org.evman.application;
 
 import android.app.Application;
 
